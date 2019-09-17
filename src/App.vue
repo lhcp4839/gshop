00e9aa4d2380430df092011cd8be7a87dd6b217c
lhcp <lhcp4839@163.com>
2019-09-17 17:07:05 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <FooterGuide v-if="$route.meta.showFooter"></FooterGuide>
+  </div>
+</template>
+
+<script>
+
+import FooterGuide from './components/FooterGuide/FooterGuide'
+export default {
+  
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus">
+  @import url('http://at.alicdn.com/t/font_518606_6676bmcalnrhehfr.css')
+  @import './static/css/reset.css'
+  
+</style>
+/* 70 */
